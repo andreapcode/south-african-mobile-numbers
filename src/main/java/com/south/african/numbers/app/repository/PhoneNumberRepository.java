0cbf23eef9687a672886cecb11c1dbe4bd28e5ec
andreapcode <aiwass90@gmail.com>
2023-11-30 15:09:06 +0100

    feat: add annotation

--- src/main/java/com/south/african/numbers/app/repository/PhoneNumberRepository.java
+++ src/main/java/com/south/african/numbers/app/repository/PhoneNumberRepository.java
@@ -2,14 +2,15 @@ package com.south.african.numbers.app.repository;
 
 import com.south.african.numbers.app.entity.PhoneNumberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PhoneNumberRepository extends JpaRepository<PhoneNumberEntity, Long> {
 
-    public List<PhoneNumberEntity> findAllByOrderByStatusAsc();
+    List<PhoneNumberEntity> findAllByOrderByStatusAsc();
 
-    public List<PhoneNumberEntity> findByStatus(String status);
+    List<PhoneNumberEntity> findByStatus(String status);
 
-    public PhoneNumberEntity findByPhoneNumber(String phoneNumber);
+    PhoneNumberEntity findByPhoneNumber(String phoneNumber);
 }
